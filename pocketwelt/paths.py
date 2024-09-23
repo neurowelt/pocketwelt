367a5d541c76a604c7ff34d67ff12257394e72dc
neurowelt <kamilklecha96@gmail.com>
2024-09-23 13:13:58 +0200

    feat: dictionary walking methods

--- pocketwelt/paths.py
+++ pocketwelt/paths.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import Any, Dict, List, Optional
 
 
 def get_parent_path(path: str, level: int = 1, absolute: bool = True) -> str:
@@ -39,3 +39,85 @@ def list_all_paths(directory: str) -> List[str]:
             paths.append(os.path.join(root, name))
             
     return paths
+
+def build_all_paths(structure: Dict[str, Any], current_path: Optional[list] = None,
+                    result: Optional[dict] = None) -> Dict[str, Any]:
+    """
+    Recursively build a dictionary of all paths from a nested dictionary.
+
+    Args:
+        structure (Dict[str, Any]): The nested dictionary structure to process.
+        current_path (Optional[list], optional): The current path being processed. 
+            Defaults to `None`.
+        result (Optional[dict], optional): The dictionary to store the results.
+            Defaults to `None`.
+
+    Returns:
+        Dict[str, Any]: A dictionary containing all paths from the nested dictionary.
+    """
+    if result is None:
+        result = dict()
+    if current_path is None:
+        current_path = list()
+
+    for key, value in structure.items():
+        if isinstance(value, dict):
+            build_all_paths(value, current_path + [key], result)
+        else:
+            result[key] = current_path
+            for item in value:
+                result[item] = current_path + [key]
+
+    return result
+
+def build_top_paths(structure: Dict[str, Any], current_path: Optional[str] = None) -> List[str]:
+    """
+    Recursively build a list of all possible paths for a given nested dictionary.
+
+    Args:
+        structure (Dict[str, Any]): The nested dictionary structure to process.
+        current_path (Optional[str], optional): The current path being processed.
+            Defaults to `None`.
+
+    Returns:
+        List[str]: A list of all possible paths from the nested dictionary.
+    """
+    paths = []
+    for key, value in structure.items():
+        if isinstance(value, dict):
+            for sub_path in build_top_paths(value, key):
+                paths.append(f"{sub_path}")
+        else:
+            for item in value:
+                paths.append(
+                    f"{key}.{item}"
+                    if current_path is None
+                    else f"{current_path}.{key}.{item}"
+                )
+
+    return paths
+
+def get_all_attributes(structure: Dict[str, Any], attrs: Optional[List[str]] = None) -> List[str]:
+    """
+    Recursively retrieve all names that exist in the nested dictionary.
+
+    Args:
+        structure (Dict[str, Any]): The nested dictionary structure to process.
+        attrs (Optional[List[str]], optional): The list to store the attribute names.
+            Defaults to `None`.
+
+    Returns:
+        List[str]: A list of all unique attribute names in the nested dictionary.
+    """
+    if attrs is None:
+        attrs = []
+
+    for key, value in structure.items():
+        attrs.append(key)
+        if isinstance(value, dict):
+            get_all_attributes(value, attrs)
+        elif isinstance(value, list):
+            for item in value:
+                attrs.append(item)
+
+    return list(set(attrs))
